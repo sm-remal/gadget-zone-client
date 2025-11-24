da23126d2304f3d087c6869a3ffaa6d4ead243a2
sm-remal <remal.mozzem@gmail.com>
2025-11-24 22:45:49 +0600

    Next.js project setup

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,5 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/Navbar/Navbar";
+import Footer from "@/components/Footer/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -22,7 +24,13 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <div className="flex flex-col min-h-screen">
+          <Navbar></Navbar>
+          <div className="flex-1">
+            {children}
+          </div>
+          <Footer></Footer>
+        </div>
       </body>
     </html>
   );
