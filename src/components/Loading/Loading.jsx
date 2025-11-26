b01b0c204a1df07b8dee831f9cd8a5119efb59c0
sm-remal <remal.mozzem@gmail.com>
2025-11-27 00:43:14 +0600

    all-products page update

--- src/components/Loading/Loading.jsx
+++ src/components/Loading/Loading.jsx
@@ -4,7 +4,7 @@ import { HashLoader } from 'react-spinners';
 const Loading = () => {
     return (
         <div className='flex justify-center items-center min-h-screen'>
-            <HashLoader className='text-orange-500' />
+            <HashLoader color='#EA580C' />
         </div>
     );
 };
